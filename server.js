d92bc298299057109205ffc99dc86cc48849ad9b
= <=>
2013-12-13 19:16:31 +0100

    frontend - preview and basic functionality

--- server.js
+++ server.js
@@ -76,6 +76,10 @@ app.delete('/api/todos/:todo_id', function(req, res) {
     });
 });
 
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
